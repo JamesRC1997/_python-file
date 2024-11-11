6e0f6772d7c44234e82dee34cada8a4575867010
JamesRC1997 <jcunningham28@hawkmail.hccfl.edu>
2024-11-11 14:05:26 -0500

    all code done

---
+++ COP1000/Module 5/Assignment 5-1 Determine the Output of the Pseudocode.py
@@ -0,0 +1,38 @@
+#determining pseudocode output
+a = 1
+b = 2
+c = 5
+while a <c:
+    a += 1
+    b += c
+print (a, b, c,)
+d = 4
+e = 6
+f = 7
+while d > f:
+    d += 1
+    e -= 1
+print (d, e, f,)
+g = 4
+h = 6
+while g < h:
+    g += 1
+print (g, h,)
+j = 2
+k = 5
+m = 6
+n = 9
+while j < k:
+    while m < n:
+        print("Hello")
+        m += 1
+    j += 1
+p = 2
+q = 4
+while p < q:
+    print("Adios")
+    r = 1
+    while r < q:
+        print("Adios")
+        r += 1
+    p += 1
