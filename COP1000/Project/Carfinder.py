c5cdd1071bf11d4e5027ed5ec3aa91b325707b63
JamesRC1997 <jcunningham28@hawkmail.hccfl.edu>
2024-11-14 11:37:10 -0500

    part 3 workin fine

--- COP1000/Project/Carfinder.py
+++ COP1000/Project/Carfinder.py
@@ -7,7 +7,8 @@ def print_menu():
     print("Please Enter the following number below from the following menu:")
     print("1. PRINT all Authorized Vehicles")
     print("2. SEARCH for Authorized Vehicles")
-    print("3. Exit")
+    print("3. ADD Authorized Vehicle")
+    print("4. Exit")
 def print_vehicles():
     print("\nThe AutoCountry sales manager has authorized the purchase and selling of the following vehicles:")
     for vehicle in AllowedVehiclesList:
@@ -17,6 +18,12 @@ def search_vehicle(vehicle_name):
         print(f"{vehicle_name} is an authorized vehicle")
     else:
         print(f"{vehicle_name} is not an authorized vehicle, if you received this in error please check the spelling and try again")
+def add_vehicle(vehicle_name):
+    if vehicle_name not in AllowedVehiclesList:
+        AllowedVehiclesList.append(vehicle_name)
+        print(f'You have added "{vehicle_name}" as an authorized vehicle')
+    else:
+        print(f'"{vehicle_name}" is already an authorized vehicle')
 
 def main():
     while True:
@@ -28,6 +35,9 @@ def main():
             vehicle_name = input("Please Enter the full Vehicle name:\n")
             search_vehicle(vehicle_name)
         elif choice == '3':
+            vehicle_name = input("Please Enter the full Vehicle name you would like to add:\n")
+            add_vehicle(vehicle_name)       
+        elif choice == '4':
             print("Thank you for using the AutoCountry Vehicle Finder, good-bye!")
             break
         else:
